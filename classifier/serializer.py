e56c0a158d5edd6edd26c757739e23ef4adc680b
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-09-08 18:19:08 +0200

    added serializer for user registration

--- classifier/serializer.py
+++ classifier/serializer.py
@@ -1,4 +1,18 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 class PredictSerializer(serializers.Serializer):
     text=serializers.CharField()
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model=User
+        fields=["username","email","password"]
+    
+    def create(self,validated_data):
+        user=User.objects.create(
+            username=validated_data["user"],
+            email = validated_data["email"],
+            password=validated_data["password"]
+        )
+        return user
