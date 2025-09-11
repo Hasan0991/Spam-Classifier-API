93f3e05074fe6423d432a7579e03681b89e17f02
Hasan Hamzayev <gamzayevgasan098@gmail.com>
2025-09-11 17:38:35 +0200

    separated api and page  views for better workflow

--- classifier/urls.py
+++ classifier/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import PredictView,RegisterView,LoginView   
+from .views import PredictApiView,RegisterView,LoginApiView   
 from . import views
 
 urlpatterns=[
-    path("predict/",PredictView.as_view(),name="predict"),
+    path("predict/",PredictApiView.as_view(),name="predict"),
     path("register/",RegisterView.as_view(),name="register_api"),
-    path("login/",LoginView.as_view(),name="login_api")
+    path("login/",LoginApiView.as_view(),name="login_api")
 
 ]           
